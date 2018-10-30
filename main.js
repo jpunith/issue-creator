7c04dff5da8a04847a90ffc6a144cd1f30a6a523
jpunith <punithjain@outlook.com>
2018-10-30 09:29:00 +0530

    review changes

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 (function(){
     var repoName = "";
     var userName;
-    $(document).ready(function () {
+    $(document).ready(() => {
         $('.get-repos').on('click', getRepos);
-        $('.repo-list').on('click', '.card-link', openIssueModal)
-        $('.create-issue').on('click', submitIssue)
+        // $('.repo-list').on('click', '.card-link', openIssueModal)
+        // $('.create-issue').on('click', submitIssue)
     })
     
     function getRepos(event) {
         userName = $('#username').val();
         $.get('https://api.github.com/users/'+userName+'/repos')
-            .done(function(data) {
-                var template = '';
+            .done((data) => {
+                let template = '';
                 template += data.map(function(repo){
                     return '<li class="list-group-item">'+
-                            repo.name + '<a href="#" class="card-link" data-reponame="'+repo.name+'"> New Issue </a></li>'
+                            repo.name + '<a href="https://github.com/jpunith/'+repo.name+'/issues/new" class="card-link" > New Issue </a></li>'
                 }).join('')
     
                 $('.repo-list').html(template);
             })
-            .fail(function(error) {
+            .fail((error) => {
                 if(error.state() === 'rejected'){
                     $('.repo-list').html('<li class="list-group-item list-group-item-danger">Request rejected</li>');
                 }
@@ -29,56 +29,56 @@
             })
     }
     
-    function openIssueModal(event){
-        event.preventDefault();
-        repoName = $(event.target).data('reponame');
-        $('#formIssueModal').modal('show');
-    }
+    // function openIssueModal(event){
+    //     event.preventDefault();
+    //     repoName = $(event.target).data('reponame');
+    //     $('#formIssueModal').modal('show');
+    // }
     
-    function submitIssue(event){
+    // function submitIssue(event){
 
-        var parameters = {};
+    //     var parameters = {};
 
-        if($('#title').val()){
-            parameters.title = $('#title').val().trim();
-        }
-        if(!$('#password').val()){
-            alertErrorMessage("Please enter the password");
-            $('#password').closest('.col-md-6').find('.invalid-tooltip').show();
-            return;
-        }
-        if($('#body').val()){
-            parameters.body = $('#body').val().trim();
-        }
-        if($('#assignees').val()){
-            parameters.assignees = $('#assignees').val().split(',').map(function(s){return s.trim()});
-        }
-        if($('#milestone').val()){
-            parameters.milestone = Number($('#milestone').val().trim());
-        }
-        if($('#labels').val()){
-            parameters.labels = $('#labels').val().split(',').map(function(s){return s.trim()});
-        }
+    //     if($('#title').val()){
+    //         parameters.title = $('#title').val().trim();
+    //     }
+    //     if(!$('#password').val()){
+    //         alertErrorMessage("Please enter the password");
+    //         $('#password').closest('.col-md-6').find('.invalid-tooltip').show();
+    //         return;
+    //     }
+    //     if($('#body').val()){
+    //         parameters.body = $('#body').val().trim();
+    //     }
+    //     if($('#assignees').val()){
+    //         parameters.assignees = $('#assignees').val().split(',').map(function(s){return s.trim()});
+    //     }
+    //     if($('#milestone').val()){
+    //         parameters.milestone = Number($('#milestone').val().trim());
+    //     }
+    //     if($('#labels').val()){
+    //         parameters.labels = $('#labels').val().split(',').map(function(s){return s.trim()});
+    //     }
     
-        $.ajax({
-            url: 'https://api.github.com/repos/'+ userName +'/'+ repoName +'/issues',
-            headers: {
-                'Accept': 'application/vnd.github.symmetra-preview+json'
-            },
-            type:"post",
-            data: JSON.stringify(parameters),
-            dataType: 'json',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader ("Authorization", "Basic " + btoa(userName + ":" + $('#password').val()));
-            }
-        })
-        .done(function(data){
-            alertSuccessMessage("successfully created issue Issue number "+ data.number);
-        })
-        .fail(function(error){
-            alertErrorMessage(error.responseJSON.message);
-        })
-    }
+    //     $.ajax({
+    //         url: 'https://api.github.com/repos/'+ userName +'/'+ repoName +'/issues',
+    //         headers: {
+    //             'Accept': 'application/vnd.github.symmetra-preview+json'
+    //         },
+    //         type:"post",
+    //         data: JSON.stringify(parameters),
+    //         dataType: 'json',
+    //         beforeSend: function (xhr) {
+    //             xhr.setRequestHeader ("Authorization", "Basic " + btoa(userName + ":" + $('#password').val()));
+    //         }
+    //     })
+    //     .done(function(data){
+    //         alertSuccessMessage("successfully created issue Issue number "+ data.number);
+    //     })
+    //     .fail(function(error){
+    //         alertErrorMessage(error.responseJSON.message);
+    //     })
+    // }
 
     function alertSuccessMessage(msg){
         $('.alert-message').text(msg);
